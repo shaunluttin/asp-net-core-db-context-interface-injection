233fb3d1f9dd52858ef64da4ac4f21bfa4e1e300
Shaun Luttin <shaunluttin@gmail.com>
2018-07-26 17:13:24 -0700

    Remove unused namespaces and code

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using JetBrains.Annotations;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace temp
 {
@@ -25,7 +13,7 @@ namespace temp
             services.AddTransient<IRepository, Repository>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) { }
+        public void Configure() { }
     }
 
     public interface IdbContext { }
