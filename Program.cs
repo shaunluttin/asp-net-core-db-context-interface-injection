51ee58fac99aabbc33d53b2cfaff75f0dec7ff03
Shaun Luttin <shaunluttin@gmail.com>
2018-07-26 17:02:48 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace temp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = CreateWebHostBuilder(args);
+            var host = builder.Build();
+
+            using(var scope = host.Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<IdbContext>();
+                var repo = services.GetRequiredService<IRepository>();
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace temp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<IdbContext, dbContext>(options => options
+                .UseSqlServer("Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;"));
+
+            services.AddTransient<IRepository, Repository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) { }
+    }
+
+    public interface IdbContext { }
+
+    public class dbContext : DbContext, IdbContext
+    {
+        public dbContext(DbContextOptions<dbContext> options) : base(options) { }
+    }
+
+    public class Repository : IRepository
+    {
+        private readonly IdbContext _context;
+
+        public Repository(IdbContext context)
+        {
+            _context = context;
+        }
+    }
+
+    public interface IRepository
+    {
+    }
+}
